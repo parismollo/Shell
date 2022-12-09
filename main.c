467dca8704f9b825b281831c025c03bb186a057e
paris <parismollochristondis@gmail.com>
2022-12-09 11:50:57 +0100

    add ascii codes in prompt + use sigaction instead of signal

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include "slash.h"
 
 int main() {
-  signal(SIGINT, catchSignal);
+
+  struct sigaction action;
+  memset(&action, 0, sizeof(struct sigaction));
+  action.sa_handler = catchSignal;
+  sigaction(SIGINT, &action ,NULL);
+
+  // signal(SIGINT, catchSignal);
+
   while(exec_loop) {
     // Step 1: Read input and update prompt line variable:
     slash_read();
